afe790906231f37da8ff22321ba5d05c82b85cb5
Harsh Sharma <62948978+Altairian7@users.noreply.github.com>
2022-12-10 00:08:56 +0530

    FIrstCommit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <math.h>
+using namespace std;
+
+
+// int sum(int a, int b)
+// {
+// 	int result=a+b;
+// 	return result;					
+// }
+
+int main()
+{
+	
+	cout<<"Zzzzzzzyyyy";
+
+	// int x = 4;
+	// int y = 2;
+	// cout<<&x<<endl;
+	// cout<<&y<<endl;
+
+
+	// int n;
+	// cin >> n;
+	// int n_sum=0;
+	// while(n){
+	// 	n_sum += n%10;
+	// 	n = n/10;
+	// }
+	// cout<<n_sum;
+
+}
